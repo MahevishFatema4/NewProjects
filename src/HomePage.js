f0948913c4fcd518305d0bdaaae52f69c6c64362
mahevishfatema123 <mahevishfatema4@gmail.com>
2022-11-11 03:22:44 +0530

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,87 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import "./HomePage.css";
+const HomePage = () => {
+  return (
+    <div className="Mhome">
+      <div className="home">
+        <Typography className="Menu">MENU</Typography>
+        <img src="./Ellipse 37.png" alt="" className="Elli" />
+        <img src="./toit-logo 1.png" alt="" className="logo" />
+        <Typography className="Menu1">kudix</Typography>
+        <Typography className="Menu2">Reservation</Typography>
+        <Typography className="Menu3">brewery tour</Typography>
+        <Typography className="Menu4">
+          Find My Beer{" "}
+          <span>
+            <img src="./Vector (3).png" alt="" />
+          </span>{" "}
+        </Typography>
+        <img src="./TOIT.png" alt="" className="TOIT" />
+        <img src="./BEER.png" alt="" className="BEER" />
+        <div>
+          <img src="./Rectangle 4.png" alt="" className="Rectangle1" />
+          <img src="./Rectangle 5.png" alt="" className="Rectangle2" />
+          <img src="./Rectangle 6.png" alt="" className="Rectangle3" />
+          <img
+            src="./117016359-black-and-white-dynamic-waves-vector-squiggle-waving-lines-abstract-op-art-design-monochrome-wave-pa 1.png"
+            alt=""
+            className="Rectangle10"
+          />
+
+          <img
+            src="./We are Toit. You should be too….png"
+            alt=""
+            className="log"
+          />
+          <img src="./Toit-Pune-brewpub-10 2.png" alt="" className="Toit" />
+          <Typography className="lor">
+            Toit is a brew pub, brewing a revolutionary culture. We started out
+            as a sweet little Bengaluru brewing culture with the promise of some
+            bodacious brews, fabulous foods and a supreme brew-pub experience
+          </Typography>
+          <Typography className="Brew">TOIT BREWERY</Typography>
+          <img src="./Rectangle 137.png" alt="" className="Rectangle137" />
+          <img src="./Vector 5.png" alt="" className="Vector5" />
+          <img
+            src="./69562366_481182529128457_5849706215813260445_n_1570080194097 1.png"
+            alt=""
+            className="Glass"
+          />
+          <img
+            src="./Come by. Try everything twice..png"
+            alt=""
+            className="ComeBy"
+          />
+          <img src="./Love.png" alt="" className="ComeBy1" />
+          <img
+            src="./Free_Beer_Bottle_Mockup_1 (1) 1.png"
+            alt=""
+            className="Bottom"
+          />
+          <img src="./Rectangle 133.png" alt="" className="Rectangle133" />
+          <img src="./Rectangle 134.png" alt="" className="Rectangle134" />
+          <img
+            src="./Want To purchase our merchandise.png"
+            alt=""
+            className="Want"
+          />
+          <img
+            src="./f33a2360b6d5ab77eea301bbfb96957d 1.png"
+            alt=""
+            className="Cap"
+          />
+          <Typography className="lore">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </Typography>
+          <h1>hello</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
